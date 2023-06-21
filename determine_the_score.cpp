a60c99d58bcdd1e8b06f14671bba2a02c122a5df
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-06-22 00:10:32 +0600

    determine_the_score

---
+++ determine_the_score.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,n;
+        cin>>x;
+        cin>>n;
+        cout<<(x/10)*n<<endl;
+    }
+}
