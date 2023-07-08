9b1b67c1faf20ab0449b69e27a6bb97d781bfac3
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-07-08 12:01:10 +0600

    enough_space.cpp

---
+++ enough_space.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,x,y;
+	    cin>>n>>x>>y;
+	    if(n>=(x+y*2))
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
