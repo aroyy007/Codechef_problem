4b0d5d92ae4c227d06794ddef0bccef046226d9a
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-07-14 01:24:54 +0600

    Problems_in_your_to-do_list.cpp

---
+++ Problems_in_your_to-do_list.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,c=0;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+	    for(int i=0;i<n;i++)
+	    {
+	        if(a[i]>=1000)
+	        {
+	            c++;
+	        }
+	    }
+	    cout<<c<<endl;
+	}
+	return 0;
+}
