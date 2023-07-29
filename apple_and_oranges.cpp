bf940a8148520fea484f935b84c503c2f2becf09
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-07-29 23:59:06 +0600

    apple_and_oranges.cpp

---
+++ apple_and_oranges.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	int a,b;
+	cin>>a>>b;
+	if((a+b)<=x)
+	{
+	    cout<<"YES"<<endl;
+	}
+	else
+	{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
