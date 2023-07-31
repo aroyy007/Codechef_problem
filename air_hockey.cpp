6f1fe8e1da092a24a3af4a8e5c4b16f62de3ff86
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-07-31 11:20:34 +0600

    air_hockey.cpp

---
+++ air_hockey.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c;
+	    cin>>a>>b;
+	    c=max(a,b);
+	    cout<<(7-c)<<endl;
+	}
+	return 0;
+}
