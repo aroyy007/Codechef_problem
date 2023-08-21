6939490be8e7b2172ed92a34199414f80a4c8487
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-08-22 02:54:45 +0600

    monthly_budget

---
+++ monthly_budget.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    if(x>=y*30)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	
+}
