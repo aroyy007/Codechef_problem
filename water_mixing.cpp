2a4bd4707a71a4ed858fb134556f18e0b57d3ae0
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-08-31 23:56:59 +0600

    water_mixing

---
+++ water_mixing.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,x,y;
+	    cin>>a>>b>>x>>y;
+	    
+	    if(a==b)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else if(a<b)
+        {
+            if(x>=b-a)
+            cout<<"YES"<<endl;
+            else 
+            cout<<"NO"<<endl;
+        }
+        else if(b<a)
+        {
+            if(y>=a-b)
+            cout<<"YES"<<endl;
+            else
+            cout<<"NO"<<endl;
+        }
+	}
+	return 0;
+}
