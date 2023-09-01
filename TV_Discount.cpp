fd90ce8f11414c5dfc2ed0389561aad44f8e194e
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-09-02 00:24:46 +0600

    TV_Discount

---
+++ TV_Discount.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    
+	    if((a-c)>(b-d))
+	    {
+	        cout<<"Second"<<endl;
+	    }
+	    else if((a-c)<(b-d))
+	    {
+	        cout<<"First"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"Any"<<endl;
+	    }
+	}
+	
+	return 0;
+}
