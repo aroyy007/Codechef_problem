8133ffa3e4ba586abf68795b2cf1da42224de711
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-09-01 22:55:06 +0600

    maximum_submission

---
+++ maximum_submission.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,a;
+	    cin>>x;
+	    a=(x*60);
+	    
+	    cout<<a/30<<endl;
+	}
+	return 0;
+}
