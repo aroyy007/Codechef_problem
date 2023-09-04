41b739c4069880436dafc474fd714b5e2ac3658c
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-09-04 20:57:10 +0600

    ada_school

---
+++ ada_school.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>>n>>m;
+	    //int a[n][m];
+	    
+	    if((n*m)%2==0)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
