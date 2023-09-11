52b84b97eda1516d1799ad979d732e77f9fb1df2
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-09-12 01:37:07 +0600

    Best_Coupon

---
+++ Best_Coupon.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    
+	    if((x*0.1)>(100))
+	    {
+	        cout<<x*0.1<<endl;
+	    }
+	    else
+	    {
+	        cout<<100<<endl;
+	    }
+	}
+	return 0;
+}
