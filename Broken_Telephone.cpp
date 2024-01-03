d755e1f4c469c80e3ff4a4af286a88982c523819
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-01-03 11:53:33 +0600

    Broken_Telephone

---
+++ Broken_Telephone.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+using namespace std;
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        ain(a,n);
+        
+        int c = 0;
+        if(a[0]!=a[1])
+        {
+            c++;
+        }
+        if(a[n-1]!=a[n-2])
+        {
+            c++;
+        }
+        for(int i=1;i<n-1;i++)
+        {
+            if(a[i] != a[i + 1] || a[i] != a[i - 1])
+            {
+                c++;
+            }
+        }
+        cout<<c<<endl;
+    }
+    return 0;
+}
