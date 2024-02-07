65df062dca0aa58ebf42202d8d8ef61e04e975c4
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-02-08 00:20:00 +0600

    Valentine_Gifts

---
+++ Valentine_Gifts.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+#define endl '\n'
+#define PB push_back
+#define coy cout << "YES" << endl;
+#define con cout << "NO" << endl;
+#define ain(A, NN)              \
+    for (ll I = 0; I < NN; I++) \
+    cin >> A[I]
+using namespace std;
+
+int main()
+{
+    optimize();
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x;
+        cin>>x;
+        
+        // int g=1;
+        // //bool flag = false;
+        
+        // int a=7,sum=0;
+        // while(a--)
+        // {
+        //     //x=x-g;
+        //     sum=sum+g;
+        //     g=g*2;
+        // }
+        
+        // if(sum<127)
+        // {
+        //     flag=true;
+        // }
+        
+        if(x<127)
+        {
+            cout<<"No"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+        }
+    }
+    
+    return 0;
+}
